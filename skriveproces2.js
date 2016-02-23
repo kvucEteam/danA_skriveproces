2e151a3d5e4e0ff58065213b096557f526aad485
Thomas <kvucthan@gmail.com>
2016-02-23 10:08:53 +0100

    pdf med Klaus Rifbjergs "Det blev os pålagt" og definitioner til "analytiske fokuspunkter" er tilføjet.

--- skriveproces2.js
+++ skriveproces2.js
@@ -915,6 +915,8 @@ $( document ).on('click', "#step_2_goOn", function(event){
 		}
 	}
 
+
+
 	console.log("step_2_goOn - fallbackStudentTheme: " + fallbackStudentTheme); 
 	console.log("step_2_goOn - jsonData.studentSelectedSubject 1: " + JSON.stringify(jsonData.studentSelectedSubject)); 
 
@@ -1179,7 +1181,7 @@ function step_5_template(){
 
 	HTML += 			'<div id="TextAndQuoteContainer">';
 			
-	HTML +=				'<span class="Texts btn btn-info" >'+jsonData.texts[JST.textNo].author+': "'+jsonData.texts[JST.textNo].title+'", '+jsonData.texts[JST.textNo].year+'</span>';
+	HTML +=				'<span class="TextRef btn btn-info" >'+jsonData.texts[JST.textNo].author+': "'+jsonData.texts[JST.textNo].title+'", '+jsonData.texts[JST.textNo].year+'</span>';
 	
 	HTML += 				'<div id="QuoteContainer" class="btnActions">';
 				for (var i = 0; i < jsonData.numOfChoosenWords; i++) {
@@ -1207,6 +1209,22 @@ function step_5_template(){
 	return HTML;
 }
 
+
+$( document ).on('click', ".TextRef", function(event){
+	$(this).removeClass('btn-primary').addClass('btn-info');
+
+	var JST = jsonData.studentSelectedTexts[jsonData.selectedTextIndexNum];
+	var JT = jsonData.texts[JST.textNo];
+
+	var HTML = '';
+    HTML += '<h1>'+JT.title+'</h1> <i>Af '+JT.author+', '+JT.year+'</i> <br/><br/>';
+    HTML += '<h4>Tekstuddrag:</h4>'
+    HTML += JT.textSnippet;
+    HTML += '<br/> <a class="btn btn-lg btn-info" href="'+JT.src+'" target="_blank">Læs pdf i nyt browser vindue</a> <a class="btn btn-lg btn-info" href="'+JT.src+'" download="'+String(JT.src.split('/').pop())+'">Download pdf</a>';
+
+    // UserMsgBox("body", '<h1>'+JT.title+'</h1> <i>Af '+JT.author+', '+JT.year+'</i><br/><br/>'+'<a href="test.pdf" target="_blank">test-pdf</a> <a href="test.pdf" download="test.pdf">Download the pdf</a> <br/> <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> <br/> <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?</p> <br/> <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat.</p>');
+    UserMsgBox("body", HTML);
+});
 
 
 $( document ).on('click', ".quoteBtn", function(event){
@@ -1399,7 +1417,7 @@ function step_6_template(){
 
 	HTML += 			'<div id="TextAndQuoteContainer">';
 			
-	HTML +=				'<span class="Texts btn btn-info" >'+jsonData.texts[JST.textNo].author+': "'+jsonData.texts[JST.textNo].title+'", '+jsonData.texts[JST.textNo].year+'</span>';
+	HTML +=				'<span class="TextRef btn btn-info" >'+jsonData.texts[JST.textNo].author+': "'+jsonData.texts[JST.textNo].title+'", '+jsonData.texts[JST.textNo].year+'</span>';
 	
 	HTML += 				'<div id="QuoteContainer" class="btnActions">';
 				for (var i = 0; i < jsonData.numOfChoosenWords; i++) {
